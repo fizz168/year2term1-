b6ddcef4668ec0ae63a493c2f235775a42928fd0
fizz168 <nheanomra@gmail.com>
2025-12-09 15:16:46 +0700

    noi

---
+++ algorithm/graph/adjm.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class GraphM{
+    private:
+    vector<vector<int>> ahjm;
+    public:
+    GraphM(int n){
+        
+    }
+
+};
--- algorithm/graph/mayrean.cpp
+++ algorithm/graph/mayrean.cpp
@@ -2,12 +2,20 @@
 #include<vector>
 using namespace std;
 int main (){
-    vector<vector<int>>n = {
-        {1,2,3},
-        {4,5,6}
+//     vector<vector<int>>n = {
+//         {1,2,3},
+//         {4,5,6}
 
-    };
-cout<<n.at(1).at(2)<<endl;
+//     };
+// cout<<n.at(1).at(2)<<endl;
+vector<vector<int>> n(3, vector<int>(3, 0));
+for(auto i: n){
+    for(auto j: i){
+        cout<<j<<" ";
+    }
+    cout<<endl;
+}
+
 
 
     return 0;
