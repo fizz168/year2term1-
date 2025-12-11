38125168a45dd58baac77958a5b958763e8f0da8
fizz168 <nheanomra@gmail.com>
2025-12-11 21:13:58 +0700

    feat: refactor css to tailwind css

--- jstest.js
+++ jstest.js
@@ -1,1 +1,31 @@
-console.log("hello")
+// function challenge1(width, height){
+//     for(let i = 0; i < height; i++){
+//         for( let j = 0; j < width; j++){
+//       process.stdout.write("* ");
+//         }
+//         console.log("");
+
+//     }
+// }
+// challenge1(5, 2);
+// let arr = [1, 2, 3, 4, 5];
+// arr.reverse();
+// console.log(arr);
+
+
+
+//challenge 3 average student 
+// function average(array){
+// let sum = 0;
+// for(let i = 0; i < number.length; i++){
+//         sum += array[i];
+// }
+//     const average = sum / number.length;
+//     return average; 
+
+// }
+
+// const number = [10, 20, 30];
+// console.log(average(number));
+
+// challenge 4
